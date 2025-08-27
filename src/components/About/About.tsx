6e85a34d7c2737f942a79dbfb697d7a3f9ab2df4
joaohenriquedsl <joaohenriquedsl2@gmail.com>
2025-08-27 18:09:17 -0300

    add about section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,24 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="about-left">
+        <h2>Prazer, sou Eduardo Vela Luiz</h2>
+        <p>
+          Com anos de experiência no mercado de climatização em Americana e
+          região, meu compromisso na Deltha Climatização é oferecer um serviço
+          técnico, honesto e de confiança. Cada instalação é tratada com o
+          máximo cuidado para garantir seu conforto e segurança.
+        </p>
+        <button className="about-cta cta">Fale Comigo</button>
+      </div>
+
+      <div className="about-right">
+        <div className="about-image"></div>
+      </div>
+    </section>
+  );
+}
+
+export default About;

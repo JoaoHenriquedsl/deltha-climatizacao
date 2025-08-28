4feb869ea6966be01ba0a0727fd613a5c59d258a
joaohenriquedsl <joaohenriquedsl2@gmail.com>
2025-08-27 21:31:55 -0300

    add AreReady call-to-action section

---
+++ src/components/Sections/AreReady/AreReady.tsx
@@ -0,0 +1,17 @@
+import Button from "../../UI/Button/Button";
+import './AreReady.css'
+
+const AreReady = () => {
+  return (
+    <section className="are-ready">
+      <h1>Pronto para climatizar seu ambiente?</h1>
+      <p>
+        Fale comigo agora e receba um orçamento gratuito e sem compromisso.
+        Atendimento rápido e profissional em Americana e região.
+      </p>
+      <Button className="ready-btn cta" href="">Falar com um Técnico no WhatsApp</Button>
+    </section>
+  );
+};
+
+export default AreReady;
